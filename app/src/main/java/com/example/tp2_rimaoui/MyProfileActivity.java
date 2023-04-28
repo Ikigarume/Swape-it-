5dfbeaf7f98f54d05defec341b63ebd77f639b8c
Ikigarume <bellarim@hotmail.fr>
2023-04-28 03:30:27 +0100

    Avancement

--- app/src/main/java/com/example/tp2_rimaoui/MyProfileActivity.java
+++ app/src/main/java/com/example/tp2_rimaoui/MyProfileActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.tp2_rimaoui.ui.home.HomeFragment;
 
@@ -14,6 +16,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 //import com.example.tp2_rimaoui.databinding.ActivityMyProfileBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 
@@ -23,53 +26,46 @@ public class MyProfileActivity extends AppCompatActivity {
     public static final String USER_KEY = "user_id";
     Bundle bundle = new Bundle() ;
     //bundle.putInt("user_id", userId);
-    private Button buttonDiscussion , buttonOffers , buttonFavorites , buttonGoBack, buttonMore;
+    private Button buttonDiscussion , buttonOffers , buttonFavorites ;
+    private ImageView ImageGoBack, ImageMore ;
     private SessionManager sessionManager ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
-        buttonDiscussion = findViewById(R.id.discussions_button);
-        buttonOffers = findViewById(R.id.offers_button);
-        buttonFavorites = findViewById(R.id.favorites_button);
-        buttonGoBack = findViewById(R.id.imageBack) ;
-        buttonMore = findViewById(R.id.imagemore) ; // to log out and to edit informations
+        ImageGoBack = findViewById(R.id.imageBack) ;
+        ImageMore = findViewById(R.id.imagemore) ; // to log out and to edit informations
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+
 
         sessionManager = new SessionManager(this);
 
         replaceFragment(new fragment_Favorites());
-        buttonDiscussion.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_Discussions:
+                        Toast.makeText(MyProfileActivity.this, "You clicking on buttondiscc", Toast.LENGTH_SHORT).show();
+                        replaceFragment(new Fragment_Discussions());
 
-                replaceFragment(new Fragment_Discussions());
-
-            }
-        });
-
-        buttonOffers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                replaceFragment(new Fragement_Offers());
-
-            }
-        });
-
-        buttonFavorites.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                replaceFragment(new fragment_Favorites());
-
-            }
-        });
-
-        buttonGoBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
+                        return true;
+                    case R.id.navigation_Offers:
+                        Toast.makeText(MyProfileActivity.this, "You clicking on buttonoffers", Toast.LENGTH_SHORT).show();
+                        replaceFragment(new Fragement_Offers());
+                        return true;
+                    case R.id.navigation_favorites:
+                        Toast.makeText(MyProfileActivity.this, "You clicking on buttonfav", Toast.LENGTH_SHORT).show();
+                        replaceFragment(new fragment_Favorites());
+                        return true;
+                    default:
+                        return false;
+                }
             }
         });
 
@@ -83,19 +79,7 @@ public class MyProfileActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frameLayout,fragment);
         fragmentTransaction.commit();
     }
-/*
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.navigation_Discussions:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-                return true;
-            case R.id.navigation_Offers:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-                return true;
-        }
-        return false;
-    }
- */
+
 
     
 }
